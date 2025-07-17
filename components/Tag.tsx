297ec73c2b86702de3b1347a1bd981ead79efbec
Kevin He <kevin66he@hotmail.com>
2025-07-17 16:59:46 -0400

    Updated tag design

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 rounded-md px-2 py-0.5 text-sm font-medium font-semibold uppercase dark:bg-gray-700"
+      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 rounded-md bg-gray-200 px-2 py-0.5 text-sm font-medium font-semibold uppercase dark:bg-gray-700"
     >
       #{text.split(' ').join('-')}
     </Link>
